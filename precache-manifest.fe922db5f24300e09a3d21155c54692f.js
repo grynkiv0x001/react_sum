5ae833682524b0367d73a2275aa199e8a1040a46
grynkiv0x001 <grynkiv0x001@gmail.com>
2021-07-15 17:29:29 +0300

    Updates

---
+++ precache-manifest.fe922db5f24300e09a3d21155c54692f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77e0594e1b3b109b1bc39bcbf83c1c9a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "183fdae1b195654a11d8",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "9cb276670027b79266c7",
+    "url": "./static/js/2.bafbf7af.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.bafbf7af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "183fdae1b195654a11d8",
+    "url": "./static/js/main.a2702802.chunk.js"
+  },
+  {
+    "revision": "c2acbe53827daed63a5f",
+    "url": "./static/js/runtime-main.1aec85e4.js"
+  }
+]);
